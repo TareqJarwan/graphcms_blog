b1999ff1fee2e9327bc8e4b7c08913b74e95b41a
TareqJarwan <tjarwan22@gmail.com>
2021-11-14 12:18:02 +0200

    fifth commit

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -5,7 +5,6 @@ import { getCategories, getCategoryPost } from '../../services';
 import { PostCard, Categories, Loader } from '../../components';
 
 const CategoryPost = ({ posts }) => {
-  console.log(posts);
   const router = useRouter();
 
   if (router.isFallback) {
